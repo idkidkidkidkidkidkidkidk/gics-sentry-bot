09d6a7ceb2e0c38b843416349179418a8845701a
idkidkidkidkidkidkidkidk <57472359+idkidkidkidkidkidkidkidk@users.noreply.github.com>
2024-03-20 22:54:12 +0800

    Use pygame instead of vlc

--- testvolume.py
+++ testvolume.py
@@ -1,6 +1,12 @@
 from time import sleep
-import vlc
+import os
 
-p = vlc.MediaPlayer("alarm.mp3")
-p.play()
-sleep(300)
+# 關掉 pygame 歡迎訊息
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+from pygame import mixer
+mixer.init()
+mixer.music.load('./alarm.mp3')
+mixer.music.play()
+while mixer.music.get_busy():
+    sleep(1)
